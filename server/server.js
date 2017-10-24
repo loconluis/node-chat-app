aed1e863fdcb7c20fa409deb3deab0eb727d1ed8
loconluis <luis.locon@gmail.com>
2017-10-23 23:33:16 -0600

    fixing port 2

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 // PORT variable
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 // static folder
 const publicPath = path.join(__dirname, '../public')
 app.use(express.static(publicPath))
