97b0b64c6cd75118f8d8cf1842d1499d2dc82087
loconluis <luis.locon@gmail.com>
2017-10-25 21:17:10 -0600

    Creating templates to uses with mustaches js

--- public/js/index.js
+++ public/js/index.js
@@ -9,24 +9,28 @@ socket.on('disconnect', function () {
 })
 
 socket.on('newMessage', function (message) {
-  // console.log('newMessage on', message)
-  var formattedTime = moment(message.createdAt).format('h:mm a')
-  var li = jQuery('<li></li>')
-  li.text(`${message.from} ${formattedTime}: ${message.text}`)
+  let formattedTime = moment(message.createdAt).format('h:mm a')
 
-  jQuery('#messages').append(li)
+  let template = jQuery('#message-template').html()
+  let html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  })
+
+  jQuery('#messages').append(html)
 })
 
 socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a')
-  var li = jQuery('<li></li>')
-  var a = jQuery('<a target="_blank">My current location</a>')
+  let template = jQuery('#location-template').html()
+  let html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  })
 
-  li.text(`${message.from}: ${formattedTime} `)
-  a.attr('href', message.url)
-  li.append(a)
-
-  jQuery('#messages').append(li)
+  jQuery('#messages').append(html)
 })
 
 // jquery forms
